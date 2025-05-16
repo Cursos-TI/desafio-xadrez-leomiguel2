40a03d158d6f2efd7defd707590669d473204e16
leomiguel2 <leonardofamosotimao@hotmail.com>
2025-05-16 22:24:04 +0000

    criei um programa de movimento de3 peças dexadrez.

--- xadrez.c
+++ xadrez.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+
 
 int main() {
-    // Nível Novato - Movimentação das Peças
+ 
+    // Nível Novato -  Mover a torre 5 casas para a direita
+        for (int i = 0; i < 5; i++) {
+        printf("direita \n"); //imprime a direção do movimento 
+        }
+   
+         for (int i = 0; i > 5; i++) {
+        printf("esquerda \n"); //imprime a direção do movimento
+       
+    }
+    return 0;
+    }
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
@@ -28,5 +36,3 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
-}
